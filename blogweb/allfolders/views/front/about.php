85397280844205cc5ec71bd383905a369b7d7556
Paoolooo <wassim.hilali@esprit.tn>
2021-05-20 21:36:59 +0100

    blog by wassim

---
+++ blogweb/allfolders/views/front/about.php
@@ -0,0 +1,173 @@
+<?php require_once 'header.php';
+?>
+<!-- header -->
+<?php require_once 'header_main.php'; ?>
+<!-- //header -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+<div class="site-blocks-cover overlay" style="background-image: url('images/hero1.jpg');">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 mx-auto align-self-center">
+        <div class="site-block-cover-content text-center">
+          <h1 class="mb-0">About <strong class="text-primary">Makelti</strong></h1>
+          <div class="row justify-content-center mb-5">
+            <div class="col-lg-6 text-center">
+              <p>Welcome</p>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="site-section py-5" data-aos="fade">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4">
+        <h3 class="text-black h4">Why Us</h3>
+        <p>Text</p>
+
+      </div>
+      <div class="col-lg-4">
+        <h3 class="text-black h4">History</h3>
+        <p>Text</p>
+
+      </div>
+      <div class="col-lg-4">
+        <h3 class="text-black h4">Awards</h3>
+        <p>Text</p>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="site-section bg-light custom-border-bottom" data-aos="fade">
+  <div class="container">
+    <div class="row justify-content-center mb-5">
+
+
+      <div class="title-section text-center col-md-7">
+        <h2>Our <strong class="text-primary">Leadership</strong></h2>
+      </div>
+
+    </div>
+    <div class="row">
+      <div class="col-md-6 col-lg-4 mb-5">
+
+        <div class="block-38 text-center">
+          <div class="block-38-img">
+            <div class="block-38-header">
+              <img src="images/wassim.png" alt="Image placeholder" class="mb-4">
+              <h3 class="block-38-heading h4">Wassim Hilali</h3>
+              <p class="block-38-subheading">CEO/Co-Founder</p>
+            </div>
+            <div class="block-38-body">
+              <p>Biography of Wassim </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 mb-5">
+        <div class="block-38 text-center">
+          <div class="block-38-img">
+            <div class="block-38-header">
+              <img src="images/aziz.png" alt="Image placeholder" class="mb-4">
+              <h3 class="block-38-heading h4">Mohamed Aziz Charrada</h3>
+              <p class="block-38-subheading">Co-Founder</p>
+            </div>
+            <div class="block-38-body">
+              <p>Biography Mohamed Aziz </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 mb-5">
+        <div class="block-38 text-center">
+          <div class="block-38-img">
+            <div class="block-38-header">
+              <img src="images/zeineb.png" alt="Image placeholder" class="mb-4">
+              <h3 class="block-38-heading h4">Zeineb Elyounssi</h3>
+              <p class="block-38-subheading">Marketing</p>
+            </div>
+            <div class="block-38-body">
+              <p> Biography of Zeineb </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 mb-5">
+        <div class="block-38 text-center">
+          <div class="block-38-img">
+            <div class="block-38-header">
+              <img src="images/bensalah.png" alt="Image placeholder" class="mb-4">
+              <h3 class="block-38-heading h4">Mohamed Ben salah</h3>
+              <p class="block-38-subheading">Sales Manager</p>
+            </div>
+            <div class="block-38-body">
+              <p> Biography of Mohamed</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-6 col-lg-4 mb-5">
+
+        <div class="block-38 text-center">
+          <div class="block-38-img">
+            <div class="block-38-header">
+              <img src="images/sadok.png" alt="Image placeholder" class="mb-4">
+              <h3 class="block-38-heading h4">Abdessadok Benjemàa</h3>
+              <p class="block-38-subheading">CEO/Co-Founder</p>
+            </div>
+            <div class="block-38-body">
+              <p>Biography of Abdessadok </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 mb-5">
+        <div class="block-38 text-center">
+          <div class="block-38-img">
+            <div class="block-38-header">
+              <img src="images/yassine.png" alt="Image placeholder" class="mb-4">
+              <h3 class="block-38-heading h4">Yassine Brahmi</h3>
+              <p class="block-38-subheading">Co-Founder</p>
+            </div>
+            <div class="block-38-body">
+              <p>Biography of Yassine </p>
+            </div>
+          </div>
+        </div>
+      </div>
+   
+    </div>
+  </div>
+</div>
+
+
+
+<?php require_once 'footer.php';
+?>
+</div>
+
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/aos.js"></script>
+
+<script src="js/main.js"></script>
+
+</body>
+
+</html>
---
+++ blogweb/allfolders/views/front/footer.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Makelti &mdash; Colorlib Template</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<footer class="site-footer bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-lg-4 mb-4 mb-lg-0">
+
+                <div class="block-7">
+                    <h3 class="footer-heading mb-4">About <strong class="text-primary">Makelti</strong></h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius quae reiciendis distinctio voluptates
+                        sed dolorum excepturi iure eaque, aut unde.</p>
+                </div>
+
+            </div>
+            
+
+            <div class="col-md-6 col-lg-3">
+                <div class="block-5 mb-5">
+                    <h3 class="footer-heading mb-4">Contact Info</h3>
+                    <ul class="list-unstyled">
+                        <li class="address">chotrana,Tunis,Tunisia</li>
+                        <li class="phone"><a href="tel://23923929210">+ 123 4567 89</a></li>
+                        <li class="email">makelti@gmail.com</li>
+                    </ul>
+                </div>
+
+
+            </div>
+        </div>
+        <div class="row pt-5 mt-5 text-center">
+            <div class="col-md-12">
+                <p>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    Copyright &copy;
+                    <script>
+                        document.write(new Date().getFullYear());
+                    </script> All rights reserved Template 
+                    with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" class="text-primary">Makelti</a>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                </p>
+            </div>
+
+        </div>
+    </div>
+</footer>
